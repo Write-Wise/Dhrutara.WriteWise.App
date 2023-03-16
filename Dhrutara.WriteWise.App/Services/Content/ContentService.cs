b8eb7bb0a043fd3e7beddf6f472a2865bc4a7580
keerthiusha <keerthiushakrishnan@outlook.com>
2023-03-15 20:52:17 -0400

    Add authorization to the content api calls.

--- Dhrutara.WriteWise.App/Services/Content/ContentService.cs
+++ Dhrutara.WriteWise.App/Services/Content/ContentService.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text.Json;
 
 namespace Dhrutara.WriteWise.App.Services.Content
@@ -20,6 +21,10 @@ namespace Dhrutara.WriteWise.App.Services.Content
                 PropertyNamingPolicy = new LowerCaseNamingPolicy(),
                 WriteIndented = true,
             };
+
+            UserContext? user = await _authService.SigninAsync(true, cancellationToken).ConfigureAwait(false);
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", $"Bearer {user?.AccessToken}");
 
             HttpResponseMessage apiResponse = await _httpClient
                 .PostAsJsonAsync("getcontent", request, options, cancellationToken)
